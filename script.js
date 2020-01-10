5100a6360a5cd1cbe592dbc88bc0c549e88678cb
Marcia <marciaadler718@gmail.com>
2020-01-10 11:57:10 -0500

    start of JavaScript

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ var todoInput = document.querySelector("#todo-text");
 var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
-
+// create string
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+
+for (let i = 0; i < todos.length; i++) {
+    var listItem = document.createElement('li');
+    listItem.textContent = todos[i];
+    todoList.appendChild(listItem);
+    console.log(todos[listItem]);  
+}
+// user types new todo and is captured
+// todoInput.addEventListener('', function(event) {
+//     event.preventDefault();
+//     console.log(todoInput.value);
+// });
